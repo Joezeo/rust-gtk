47a8765f1cec27a29d46ca66f36e1fb0ec1a2553
JoeZane <joezane.cn@gmail.com>
2022-10-09 23:09:58 +0800

    1. Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
+use gtk::{Application, ApplicationWindow, Button, Orientation};
+use std::cell::Cell;
+use std::rc::Rc;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
@@ -15,29 +17,44 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    // Create two buttons
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    let button_decrease = Button::builder()
+        .label("Decrease")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
+    // Reference-counted object with inner-mutability
+    let number = Rc::new(Cell::new(0));
 
+    // Connect callbacks, when a button is clicked `number` will be changed
+    let number_copy = number.clone();
+    button_increase.connect_clicked(move |_| number_copy.set(number_copy.get() + 1));
+    button_decrease.connect_clicked(move |_| number.set(number.get() - 1));
+
+    // Add buttons to `gtk_box`
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
-    // Present window
+    // Present the window
     window.present();
 }
