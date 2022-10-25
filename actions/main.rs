f44a2ccbe145a575de63e231433497d2d2e6f128
JoeZane <joezane.cn@gmail.com>
2022-10-26 00:08:41 +0800

    1. sample of actions

---
+++ actions/main.rs
@@ -0,0 +1,34 @@
+use glib::clone;
+use gtk::gio::SimpleAction;
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::Application;
+use gtk::ApplicationWindow;
+
+const APP_ID: &str = "com.toocol.actions";
+
+fn main() {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.set_accels_for_action("win.close", &["<Ctrl>W"]);
+
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .width_request(360)
+        .build();
+
+    let action_close = SimpleAction::new("close", None);
+    action_close.connect_activate(clone!(@weak window => move |_,_| {
+        window.close();
+    }));
+    window.add_action(&action_close);
+
+    window.present();
+}
