a5c50fc4d430c2360f1447c47e1894df5e4b8b7a
Joe Zane <joezane.cn@gmail.com>
2022-11-09 20:50:57 +0800

    1. Update to use ImageSurface

--- drawing_area/main.rs
+++ drawing_area/main.rs
@@ -1,14 +1,14 @@
 use std::{sync::atomic::AtomicI32, time::Duration};
 
 use gtk::{
-    gdk_pixbuf::Pixbuf,
-    glib::{timeout_add_local, Bytes},
+    cairo::ImageSurface,
+    glib::timeout_add_local,
     prelude::*,
     Application, ApplicationWindow, DrawingArea,
 };
 
 const SIZE: usize = 300 * 200;
-const LEN: usize = SIZE * 3;
+const LEN: usize = SIZE * 4;
 static mut RGB_BYTES: [u8; LEN] = [140; LEN];
 static DRIRECT: AtomicI32 = AtomicI32::new(1);
 
@@ -35,35 +35,35 @@ fn change_rgb(drawing_area: &DrawingArea) {
                 (RGB_BYTES[i] as i32 + DRIRECT.load(std::sync::atomic::Ordering::SeqCst)) as u8;
         }
 
-        drawing_area.set_draw_func(move |_, ctx, x, y| {
-            let pixbuf = Pixbuf::from_bytes(
-                &Bytes::from_static(&RGB_BYTES),
-                gtk::gdk_pixbuf::Colorspace::Rgb,
-                false,
-                8,
-                300,
-                200,
-                300 * 3,
-            );
-            ctx.set_source_pixbuf(&pixbuf, x as f64, y as f64);
-            ctx.paint().expect("Invalid surface.");
-            ctx.set_source_rgba(0.0, 0.0, 0.0, 0.0);
-        });
         drawing_area.queue_draw();
     }
 }
 
 fn build_ui(app: &Application) {
-    let drawing_area = DrawingArea::builder()
-        .content_width(300)
-        .content_height(200)
-        .build();
+    let drawing_area = DrawingArea::builder().build();
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Pixbuf")
         .child(&drawing_area)
         .build();
+
+    unsafe {
+        let surface = ImageSurface::create_for_data_unsafe(
+            RGB_BYTES.as_mut_ptr(),
+            gtk::cairo::Format::Rgb24,
+            300,
+            200,
+            300 * 4,
+        )
+        .expect("Create `ImageSurface` failed.");
+
+        drawing_area.set_draw_func(move |_, cr, _width, _height| {
+            cr.set_source_surface(&surface, 0., 0.).expect("Set source surface failed.");
+            cr.paint().expect("Invalid surface.");
+            cr.set_source_rgba(0.0, 0.0, 0.0, 0.0);
+        });
+    }
 
     timeout_add_local(Duration::from_millis(10), move || {
         change_rgb(&drawing_area);
