03787d0567769297c88764de9c14b984c8d8b898
JoeZane <joezane.cn@gmail.com>
2022-11-13 18:03:34 +0800

    1. Add demo of font icon

---
+++ font_icon/font_config.rs
@@ -0,0 +1,38 @@
+#![allow(dead_code)]
+use std::ffi::{c_char, CString};
+
+#[link(name = "native-fontconfig")]
+extern "C" {
+    fn load_font(font_path: *const c_char);
+    fn load_font_private(font_path: *const c_char);
+}
+
+pub struct FontConfig;
+impl FontConfig {
+    pub fn native_load_font(font_path: String) {
+        unsafe {
+            let font_path = CString::new(font_path).unwrap();
+            load_font(font_path.as_ptr())
+        }
+    }
+    pub fn native_load_font_private(font_path: String) {
+        unsafe {
+            let font_path = CString::new(font_path).unwrap();
+            load_font_private(font_path.as_ptr())
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! load_font {
+    () => {};
+    ( $($x:expr),* ) => {
+        {
+            $(
+                let mut path = "font/".to_string();
+                path.push_str($x);
+                font_config::FontConfig::native_load_font_private(path);
+            )*
+        }
+     };
+}
---
+++ font_icon/main.rs
@@ -0,0 +1,108 @@
+mod font_config;
+
+use gtk::pango::Layout;
+use gtk::{
+    gdk::Display, glib, glib::clone, pango::FontDescription, prelude::*, Application,
+    ApplicationWindow, CssProvider, DrawingArea, FontChooserDialog, Label, StyleContext,
+    STYLE_PROVIDER_PRIORITY_APPLICATION,
+};
+use pangocairo::FontMap;
+
+const APP_ID: &str = "com.font_icon";
+
+fn main() {
+    // std::env::set_var("PANGOCAIRO_BACKEND", "fc");
+    load_font!(
+        "Font-Awesome-6-Free-Regular-400.otf",
+        "SegMDL2.ttf",
+        "Segoe-Fluent-Icons.ttf"
+    );
+
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_startup(|_| {
+        let provider = CssProvider::new();
+        provider.load_from_data(include_bytes!("style.css"));
+
+        if let Some(display) = &Display::default() {
+            StyleContext::add_provider_for_display(
+                display,
+                &provider,
+                STYLE_PROVIDER_PRIORITY_APPLICATION,
+            );
+        };
+    });
+
+    app.connect_activate(build_ui);
+
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let gtk_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .spacing(10)
+        .build();
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Font Icon")
+        .default_width(600)
+        .default_height(400)
+        .build();
+
+    let dw = DrawingArea::builder()
+        .content_width(30)
+        .content_height(30)
+        .build();
+    // Seems css not work.
+    dw.add_css_class("font-icon");
+
+    let font_map = FontMap::for_font_type(gtk::cairo::FontType::FontTypeWin32).unwrap();
+    font_map.list_families().iter().for_each(|font| {
+        println!("{}", font.name());
+    });
+    let context = font_map.create_context();
+    let layout = Layout::new(&context);
+    let font_description = FontDescription::from_string("Font Awesome 6 Free light");
+
+    let text = "\u{f02e}";
+    layout.set_font_description(Some(&font_description));
+    layout.set_markup(format!("<span foreground=\"#6c698d\">{}</span>", text).as_str());
+
+    let size = 13;
+    let label_1 = Label::new(None);
+    label_1.set_markup(format!("<span foreground=\"#6c698d\" font_desc=\"Font Awesome 6 Free light {}\" font_features=\"dlig=1\">{}</span>", size, text).as_str());
+
+    let text_2 = "\u{e70b}";
+    let label_2 = Label::new(None);
+    label_2.set_markup(format!("<span foreground=\"#6c698d\" font_desc=\"Segoe Fluent Icons {}\" font_features=\"dlig=1\">{}</span>", size, text_2).as_str());
+
+    dw.set_draw_func(move |_, cr, _, _| {
+        cr.move_to(10., 10.);
+        pangocairo::show_layout(&cr, &layout);
+    });
+
+    let button = gtk::Button::builder()
+        .label("Click to change color")
+        .width_request(30)
+        .height_request(20)
+        .build();
+    button.connect_clicked(clone!(@weak label_1 => move |_| {
+        label_1.set_markup(format!("<span foreground=\"#3244df\" font_desc=\"Font Awesome 6 Free light {}\" font_features=\"dlig=1\">{}</span>", size, text).as_str());
+    }));
+
+    let font_chooser = FontChooserDialog::new(Some("Font Chooose"), Some(&window));
+
+    button.connect_clicked(clone!(@weak font_chooser => move |_| {
+        font_chooser.present();
+    }));
+
+    gtk_box.append(&dw);
+    gtk_box.append(&label_1);
+    gtk_box.append(&label_2);
+    gtk_box.append(&button);
+
+    window.set_child(Some(&gtk_box));
+    window.present();
+}
